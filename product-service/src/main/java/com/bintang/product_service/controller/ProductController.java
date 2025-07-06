30ce39a0c97c85a23194f35b15b111a93814e1e3
bintangmada <bintang.mada.suharsono@bni.co.id>
2025-07-06 07:08:14 +0700

    add product-service (entity, repository, service, controller)

---
+++ product-service/src/main/java/com/bintang/product_service/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.bintang.product_service.controller;
+
+import com.bintang.product_service.entity.Product;
+import com.bintang.product_service.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable("id") Long id){
+        return productService.getProductById(id);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+}
---
+++ product-service/src/main/java/com/bintang/product_service/entity/Product.java
@@ -0,0 +1,63 @@
+package com.bintang.product_service.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(length = 200, nullable = false, unique = false)
+    private String description;
+
+    public Product(Long id, String name, Double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
