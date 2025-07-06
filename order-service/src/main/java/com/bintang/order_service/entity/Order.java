19c09cd8933d0ee2d267d72d6e1584dcb0267f15
bintangmada <bintang.mada.suharsono@bni.co.id>
2025-07-06 07:56:28 +0700

    add order-service (entity)

---
+++ order-service/src/main/java/com/bintang/order_service/entity/Order.java
@@ -0,0 +1,12 @@
+package com.bintang.order_service.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
