094ca65b3c20a70d831b07e54e0561c422f895d8
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-07-17 09:34:23 +0700

    add generate token

---
+++ authentication-service/src/main/java/com/bintang/authentication_service/service/JwtService.java
@@ -0,0 +1,39 @@
+package com.bintang.authentication_service.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtService {
+
+    public static final String SECRET = "OSAKDMNAOSKDMNSAODINASODIANSDINASODIANSDOSMADOKMQOSI";
+    public String generateToken(String username){
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    public Key getSignKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createToken(Map<String, Object> claims, String username){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+}
