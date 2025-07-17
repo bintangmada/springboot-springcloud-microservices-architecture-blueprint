f1d64010168dbd23f9d9148db8cd600df858c35b
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-07-17 11:10:05 +0700

    add authentication manager di user controller

--- authentication-service/src/main/java/com/bintang/authentication_service/controller/UserController.java
+++ authentication-service/src/main/java/com/bintang/authentication_service/controller/UserController.java
@@ -5,6 +5,7 @@ import com.bintang.authentication_service.entity.UserInfo;
 import com.bintang.authentication_service.service.JwtService;
 import com.bintang.authentication_service.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,8 @@ public class UserController {
     private UserInfoService userInfoService;
     @Autowired
     private JwtService jwtService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public String createUser(@RequestBody UserInfo userInfo){
