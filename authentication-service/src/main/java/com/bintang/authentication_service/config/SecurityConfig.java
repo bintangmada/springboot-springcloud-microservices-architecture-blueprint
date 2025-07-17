178cc0033e3a50f2f0b2f8db792d4def2ce95c65
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-07-17 11:01:04 +0700

    add authentication manager di security config

--- authentication-service/src/main/java/com/bintang/authentication_service/config/SecurityConfig.java
+++ authentication-service/src/main/java/com/bintang/authentication_service/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.bintang.authentication_service.config;
 import com.bintang.authentication_service.service.UserInfoService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -47,5 +49,10 @@ public class SecurityConfig {
         provider.setUserDetailsService(userDetailsService());
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
 }
