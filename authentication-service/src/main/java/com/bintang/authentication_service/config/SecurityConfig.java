ce758ee9bd4d03f6612dd6a8cec8dbbc17ba9e8e
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-07-17 10:45:26 +0700

    membuat bean UserDetailsService yang diletakkan pada SecurityConfig

--- authentication-service/src/main/java/com/bintang/authentication_service/config/SecurityConfig.java
+++ authentication-service/src/main/java/com/bintang/authentication_service/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.bintang.authentication_service.config;
 
+import com.bintang.authentication_service.service.UserInfoService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,5 +32,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserInfoService();
     }
 }
