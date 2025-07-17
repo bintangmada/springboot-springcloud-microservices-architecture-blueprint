9b5e4817e86e4ea30825445edbc451a07cc632ef
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-07-17 10:52:11 +0700

    add authentication provide di security config

--- authentication-service/src/main/java/com/bintang/authentication_service/config/SecurityConfig.java
+++ authentication-service/src/main/java/com/bintang/authentication_service/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.bintang.authentication_service.config;
 import com.bintang.authentication_service.service.UserInfoService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,5 +39,13 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(){
         return new UserInfoService();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 }
