4234c77eeb79ad52fb15f0115ebcfd7f33387789
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-07-18 14:06:28 +0700

    mendaftarkan auth-service ke eureka

--- authentication-service/src/main/java/com/bintang/authentication_service/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/bintang/authentication_service/AuthenticationServiceApplication.java
@@ -2,8 +2,10 @@ package com.bintang.authentication_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // enable eureka client 
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
